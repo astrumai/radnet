b1f83420310af072d1bc26a271da8a113247b98e
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-08 21:08:16 -0800

    Added unit_test file

--- utils/unit_test.py
+++ utils/unit_test.py
@@ -1,84 +1,158 @@
-"""
-
-I can also see if I can add a log file that saves in the hyper parameter I use
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-
-file_handler = logging.FileHandler('test.log')
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-
-
-from functools import wraps
-
-
-def my_logger(original_func):
-    import logging
-    logging.basicConfig(filename='{}.log'.format(original_func.__name__), level=logging.INFO)
-
-    @wraps(original_func)
-    def wrapper(*args, **kwargs):
-        logging.INFO(
-            'Ran with args:{}, and kwargs: {}'.format(args, kwargs)
-        )
-        return original_func
-    return wrapper
-
-
-# I can write a similar timing function and wrap it using the decorator to log the time
-def my_timer(original_func):
-    import time
-
-    @wraps(original_func)
-    def wrapper(*args, **kwargs):
-        t1 = time.time()
-        result = original_func(*args, **kwargs)
-        t2 = time.time() - t1
-        print('{} ran in: {} sec'.format(original_func.__name__, t2))
-        return result
-    return wrapper
-
-
-# the following decorator is equal to
-# display = my_logger(my_timer(display_info))
-@my_logger
-@my_timer
-def display_info(name, age):
-    print('display_info ran with arguments ({}, {})'.format(name, age))
-
-"""
-
-# Test for data loader
-
-"""
+import unittest
+from preprocessing.augments import *
+import numpy as np
+from preprocessing.augments import VerticalFlip
 import os
+from preprocessing.load import DataTransformer
+from torchvision.transforms import Compose, Resize, ToTensor
+from utils.helpers import convert_2d_to_target
+import torch
+from PIL import Image
+import tifffile as tiff
+import pytest
 
 
-def segmentation_loader_test(args):
-    from preprocessing.load import SegmentationLoader
-    from torchvision.transforms import Compose, Resize, ToTensor
-    import torch
-    from PIL import Image
-    import tifffile as tiff
+class TestUNet(unittest.TestCase):
+    def test_data_transformer(self):
+        """Test for data loader"""
 
-    train_path = os.path.join(args.root_dir, 'data', 'train-volume.tif')
-    labels_path = os.path.join(args.root_dir, 'data', 'train-labels.tif')
+        root_dir = "C:\\Users\\Mukesh\\Segmentation\\U-net\\"
+        # train_path = os.path.join(args.root_dir, 'data', 'train-volume.tif')
+        # labels_path = os.path.join(args.root_dir, 'data', 'train-labels.tif')
+        train_path = os.path.join(root_dir, 'data', 'train-volume.tif')
+        labels_path = os.path.join(root_dir, 'data', 'train-labels.tif')
 
-    transform = Compose([Resize(64), ToTensor()])
-    dataset = SegmentationLoader(train_path, labels_path, transform)
-    # print("Shape of train array {} \nShape of labels array {}".format(train_arr.shape, label_arr.shape))
+        # transform = Compose([Resize(args.image_size), ToTensor()])
+        transform = Compose([Resize(64), ToTensor()])
+        dataset = DataTransformer(train_path, labels_path, transform)
+        temp = [transform(Image.fromarray((tiff.imread(train_path))[1]))]
 
-    temp = [transform(Image.fromarray((tiff.imread(train_path))[1]))]
-    if torch.all(torch.eq((dataset.__getitem__(1)[0]), (temp[0]))):
-        print("segmentation loader class test passed")
-    else:
-        raise AssertionError("The tensors are not the same")
+        self.assertEqual(torch.all(torch.eq((dataset.__getitem__(1)[0]), (temp[0]))), msg="The Tensors are not equal")
 
-######################################################
+    def test_unet(self):
+        """Test U-Net model"""
 
-"""
+        from model.u_net import UNet
 
+        model = UNet(in_channels=1,
+                     n_classes=2,
+                     depth=3,
+                     wf=6,
+                     padding=False,
+                     batch_norm=False)
+        print(model)
+
+
+class TestAugmentations(unittest.TestCase):
+    """Test if you are getting correct augmentations"""
+    def test_clip(self):
+        img = np.array(
+            [[-300, 0],
+             [100, 400]], dtype=np.float32)
+        expected = np.array(
+            [[0, 0],
+             [100, 255]], dtype=np.float32)
+        clipped = clip(img, dtype=np.uint8, maxval=255)
+        self.assertEqual(clipped, expected, msg="Clip Test Failed")
+
+    @pytest.mark.parameterize('target', ['image', 'mask'])
+    def test_vertical_flip(self, target):
+        img = np.array(
+            [[1, 1, 1],
+             [0, 1, 1],
+             [0, 0, 1]], dtype=np.uint8)
+        expected = np.array(
+            [[0, 0, 1],
+             [0, 1, 1],
+             [1, 1, 1]], dtype=np.uint8)
+        img, expected = convert_2d_to_target([img, expected], target=target)
+        flipped_img = VerticalFlip(img)
+        self.assertEqual(flipped_img, expected, msg="Vertical Flip Test Failed")
+
+    @pytest.mark.parameterize('target', ['image', 'mask'])
+    def test_horizontal_flip(self, target):
+        img = np.array(
+            [[1, 1, 1],
+             [0, 1, 1],
+             [0, 0, 1]], dtype=np.uint8)
+        expected = np.array(
+            [[1, 1, 1],
+             [1, 1, 0],
+             [1, 0, 0]], dtype=np.uint8)
+        img, expected = convert_2d_to_target([img, expected], target=target)
+        flipped_img = HorizontalFlip(img)
+        self.assertEqual(flipped_img, expected, msg="Horizontal Flip Test Failed")
+
+    @pytest.mark.parameterize('target', ['image', 'mask'])
+    def test_rotate90(self, target):
+        img = np.array(
+            [[0, 0, 1],
+             [0, 0, 1],
+             [0, 0, 1]], dtype=np.uint8)
+        expected = np.array(
+            [[1, 1, 1],
+             [0, 0, 0],
+             [0, 0, 0]], dtype=np.uint8)
+        img, expected = convert_2d_to_target([img, expected], target=target)
+        rotated_img = Rotate90(img)
+        self.assertEqual(rotated_img, expected, msg="Horizontal Flip Test Failed")
+
+    @pytest.mark.parameterize()
+    def test_elastic_transform(self, monkeypatch, interpolation):
+        image = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+        mask = np.random.randint(low=0, high=2, size=(100, 100), dtype=np.uint8)
+        monkeypatch.setattr()
+        aug = ElasticTransform(alpha=1, sigma=50, alpha_affine=50,
+                               interpolation=interpolation)
+        data = aug(image=image, mask=mask)
+        expected_image = elastic_transform_fast(image, alpha=1, sigma=50,
+                                                alpha_affine=50, interpolation=interpolation,
+                                                border_mode=cv2.BORDER_REFLECT_101,
+                                                random_state=np.random.RandomState(1111))
+        expected_mask = elastic_transform_fast(mask, alpha=1, sigma=50, alpha_affine=50,
+                                               interpolation=cv2.INTER_NEAREST,
+                                               border_mode=cv2.BORDER_REFLECT_101,
+                                               random_state=np.random.RandomState(1111))
+        self.assertEqual(data['image'], expected_image, msg="The images are not transformed properly")
+        self.assertEqual(data['mask'], expected_mask, msg="The masks are not transformed properly")
+
+    @pytest.mark.parameterize()
+    def test_grid_distortion(self, interpolation):
+        image = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+        mask = np.random.randint(low=0, high=2, size=(100, 100), dtype=np.uint8)
+        aug = GridDistortion(num_steps=1, distort_limit=(0.3, 0.3), interpolation=interpolation)
+        data = aug(image=image, mask=mask)
+        expected_image = grid_distortion(image, num_steps=1, xsteps=[1.3], ysteps=[1.3], interpolation=interpolation,
+                                         border_mode=cv2.BORDER_REFLECT_101)
+        expected_mask = grid_distortion(mask, num_steps=1, xsteps=[1.3], ysteps=[1.3], interpolation=cv2.INTER_NEAREST,
+                                        border_mode=cv2.BORDER_REFLECT_101)
+        self.assertEqual(data['image'], expected_image, msg="The images are not transformed properly")
+        self.assertEqual(data['mask'], expected_mask, msg="The masks are not transformed properly")
+
+    @pytest.mark.parameterize(['beta', 'expected'], [(0.2, 0.48), (-0.1, 0.36)])
+    def test_brightness(self, beta, expected):
+        img = np.ones((100, 100, 3), dtype=np.float32) * 0.4
+        expected = np.ones_like(img) * expected
+        img = brightness_contrast_adjust(img, beta=beta)
+        assert(img.dtype == np.dtype('float32'))
+        self.assertAlmostEqual(img, expected, msg="Contrast Test Failed")
+
+    @pytest.mark.parametrize(['alpha', 'expected'], [(1.5, 0.6), (3, 1.0)])
+    def test_contrast(self, alpha, expected):
+        img = np.ones((100, 100, 3), dtype=np.float32) * 0.4
+        expected = np.ones((100, 100, 3), dtype=np.float32) * expected
+        img = brightness_contrast_adjust(img, alpha=alpha)
+        assert(img.dtype == np.dtype('float32'))
+        self.assertAlmostEqual(img, expected, msg="Contrast Test Failed")
+
+    @pytest.mark.parameterize(['gamma', 'expected'], [(1, 0.4), (10, 0.00010486)])
+    def test_gamma_change(self, gamma, expected):
+        img = np.ones((100, 100, 3), dtype=np.float32) * 0.4
+        expected = np.ones((100, 100, 3), dtype=np.float32) * expected
+        img = gamma_transform(img, gamma=gamma)
+        assert img.dtype == np.dtype('float32')
+        assert np.allclose(img, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
