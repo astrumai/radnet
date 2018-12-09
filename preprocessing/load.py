78e636286099a60990beb3167f99456521d2092f
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-08 21:11:23 -0800

    Added do_augment function for the data transformer

--- preprocessing/load.py
+++ preprocessing/load.py
@@ -1,8 +1,7 @@
 import tifffile as tiff
-import torch
-from torch.utils.data.sampler import RandomSampler
 from torch.utils.data.dataset import Dataset
 from PIL import Image
+import numpy as np
 
 
 class DataTransformer(Dataset):
@@ -14,14 +13,11 @@ class DataTransformer(Dataset):
         a list of train_images, train_labels 4D tensor tuples
     """
 
-    def __init__(self,
-                 train_filename,
-                 labels_filename,
-                 transform=None):
-
+    def __init__(self, train_filename, labels_filename, image_transform=None, image_augmentation=None):
         self.train_filename = train_filename
         self.labels_filename = labels_filename
-        self.transform = transform
+        self.image_transform = image_transform
+        self.image_augmentation = image_augmentation
         self.len_train = tiff.imread(self.train_filename).shape[0]
 
     def __len__(self):
@@ -34,15 +30,23 @@ class DataTransformer(Dataset):
         return Image.fromarray((tiff.imread(self.labels_filename))[index])
 
     def __getitem__(self, index):
-        images = self.transform(self._read_data(index))
-        labels = self.transform(self._read_labels(index))
+        images = self._read_data(index)
+        labels = self._read_labels(index)
+
+        if self.image_augmentation:
+            images, labels = self.do_augment(images, labels)
+        if self.image_augmentation is not None:
+            images = self.image_transform(self._read_data(index))
+            labels = self.image_transform(self._read_labels(index))
 
         return [images, labels]
 
+    def do_augment(self, images, labels):
+        images = np.array(images)
+        labels = np.array(labels)
+        res = self.image_augmentation(image=images, mask=labels)
 
-def sampler(data):
-    data_sampler = RandomSampler(data)
-    data_samples = next(iter(data_sampler))
+        return [Image.fromarray(res['image']), Image.fromarray(res['mask'])]
 
-    return data_samples
+
 
