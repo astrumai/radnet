517bcd757a6182f9af59e1abce1f3d4266f2b48b
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-11 15:08:33 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-REQUIRED_PACKAGES = []
+REQUIRED_PACKAGES = ['pytorch', 'tifffile', 'pillow', 'opencv', 'scipy']
 
 setuptools.setup(
-    name="",
+    name="U-Net",
     version="1.0.0",
     author="Mukesh Mithrakumar",
     author_email="mukesh.mithrakumar@jacks.sdstate.edu",
-    description="",
+    description="PyTorch implementation of U-Net for biomedical image segmentation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mukeshmithrakumar/",
@@ -27,9 +27,9 @@ setuptools.setup(
     install_requires=REQUIRED_PACKAGES,
     entry_points={
         'console_scripts': [
-            'retinanet_task = keras_retinanet.trainer.task:main',
-            'retinanet_train = keras_retinanet.trainer.train:main',
-            'retinanet_evaluate = keras_retinanet.trainer.evaluate:main',
+            'u_net_task = U-Net.trainer.task:main',
+            'u_net_train = U-Net.trainer.train:train',
+            'u_net_evaluate = U-Net.trainer.evaluate:evaluate',
         ]
     },
     python_requires='>=3',
