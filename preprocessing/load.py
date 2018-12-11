4dea791dc1c8bd608af12e4a1f40b6210bced140
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-11 14:56:42 -0800

    modified to add test data

--- preprocessing/load.py
+++ preprocessing/load.py
@@ -30,18 +30,23 @@ class DataTransformer(Dataset):
         return Image.fromarray((tiff.imread(self.labels_filename))[index])
 
     def __getitem__(self, index):
-        images = self._read_data(index)
-        labels = self._read_labels(index)
+        if self.labels_filename is not None:
+            images = self._read_data(index)
+            labels = self._read_labels(index)
 
-        if self.image_augmentation is not None:
-            print("Aug")
-            image_array = np.array(images)
-            label_array = np.array(labels)
-            aug_images = self.image_augmentation(image=image_array, mask=label_array)
-            trans_images = Image.fromarray(aug_images["image"])
-            trans_labels = Image.fromarray(aug_images["mask"])
-        if self.image_augmentation is None:
+            if self.image_augmentation is not None:
+                print("Aug")
+                image_array = np.array(images)
+                label_array = np.array(labels)
+                aug_images = self.image_augmentation(image=image_array, mask=label_array)
+                trans_images = Image.fromarray(aug_images["image"])
+                trans_labels = Image.fromarray(aug_images["mask"])
+            if self.image_augmentation is None:
+                trans_images = self.image_transform(images)
+                trans_labels = self.image_transform(labels)
+            return [trans_images, trans_labels]
+        if self.labels_filename is None:
+            images = self._read_data(index)
             trans_images = self.image_transform(images)
-            trans_labels = self.image_transform(labels)
-        return [trans_images, trans_labels]
+            return trans_images
 
