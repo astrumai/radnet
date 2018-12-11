29e93a4ac289595a6a3561831cc508de13f87c0d
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-10 19:11:24 -0800

    finished validation loop/dice

--- trainer/train.py
+++ trainer/train.py
@@ -5,11 +5,13 @@ import os
 from torchvision.transforms import Compose, Resize, ToTensor
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
+from torch import nn
 import torch
 from torch.optim import Adam
 from preprocessing.augments import augmentations
-
 from sklearn.model_selection import train_test_split
+from utils.helpers import pred_to_numpy, to_numpy
+from utils.metrics import dice
 import numpy as np
 
 import tifffile as tiff
@@ -27,6 +29,30 @@ logging.basicConfig(filename='train.log', level=logging.INFO,
 # CUDA for PyTorch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# create the loss function
+loss_bce = nn.BCEWithLogitsLoss()
+loss_values = []
+THRESHOLD = 0.5
+
+
+def validate_model(model, loader, threshold):
+    """Contains the validation loop"""
+    model = model.eval()
+    for i, data in enumerate(loader):
+        val_batch, val_labels = data
+        val_batch, val_labels = val_batch.to(device), val_labels.to(device)
+
+        # call the u-net module
+        prediction = model(val_batch)
+
+        # call the loss function
+    loss = loss_bce(prediction, val_labels)
+    pred_labels, true_labels = pred_to_numpy(prediction) > threshold, to_numpy(val_labels) > threshold
+    dices = []
+    for _pred, _labels in zip(pred_labels, true_labels):
+        dices.append(dice(_pred, _labels))
+    return np.array(dices).mean(), loss.detach().cpu().numpy()
+
 
 def train(args):
     """ Contains the training loop"""
@@ -41,18 +67,16 @@ def train(args):
     # choose between augmentations for train data
     if args.augment == 'yes':
         train_augment = augmentations()
-        train_transform = DataTransformer(data_path, labels_path,
-                                          image_transform=train_data, image_augmentation=train_augment)
+        train_transform = DataTransformer(data_path, labels_path, image_transform=train_data,
+                                          image_augmentation=train_augment)
 
     elif args.augment == 'no':
         # transforming the train data and returning a 4D tensor
-        train_transform = DataTransformer(data_path, labels_path,
-                                          image_transform=train_data, image_augmentation=None)
+        train_transform = DataTransformer(data_path, labels_path, image_transform=train_data, image_augmentation=None)
 
     # transform for validation data
     val_data = Compose([Resize(args.image_size), ToTensor()])
-    val_transform = DataTransformer(data_path, labels_path,
-                                    image_transform=val_data, image_augmentation=None)
+    val_transform = DataTransformer(data_path, labels_path, image_transform=val_data, image_augmentation=None)
 
     # split the train and validation indices
     train_indices, validation_indices = train_test_split(range(len(train_transform)), test_size=0.15)
@@ -62,53 +86,38 @@ def train(args):
     validation_samples = SequentialSampler(validation_indices)
 
     # load train and validation data
-    train_loader = DataLoader(train_transform,
-                              batch_size=args.batch_size,
-                              sampler=train_samples)
-
-    val_loader = DataLoader(val_transform,
-                            batch_size=args.batch_size,
-                            sampler=validation_samples)
+    train_loader = DataLoader(train_transform, batch_size=args.batch_size, sampler=train_samples)
+    val_loader = DataLoader(val_transform, batch_size=args.batch_size, sampler=validation_samples)
 
     # create the model
-    model = UNet(in_channels=1,
-                 n_classes=args.n_classes,
-                 depth=args.depth,
-                 wf=6,
-                 padding=False,
-                 batch_norm=False,
+    model = UNet(in_channels=1, n_classes=args.n_classes, depth=args.depth, wf=6, padding=True, batch_norm=False,
                  up_mode=args.up_mode).to(device)
 
     # create the optimizer
     optim = Adam(model.parameters())
 
     # start the training loop
-    for e in args.epochs:
-        epoch_loss = 0.0
-
+    for e in range(args.epochs):
         for i, data in enumerate(train_loader):
             train_batch, train_labels = data
             train_batch, train_labels = train_batch.to(device), train_labels.to(device)
 
             # call the u-net module
-            print(model)
             prediction = model(train_batch)
 
-            # zero the parameter gradients
-            optim.zero_grad()
-
             # call the loss function
-            loss = F.cross_entropy(prediction, train_labels)
+            loss = loss_bce(prediction, train_labels)
+            loss_values.append(loss)
 
-            # backprop the loss
+            # backward pass
+            optim.zero_grad()
             loss.backward()
             optim.step()
 
             # print the loss
-            print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(e,
-                                                               i,
-                                                               len(train_loader),
-                                                               loss.item()))
+            print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(e, i, len(train_loader), loss.item()))
+        mean_dice, val_loss = validate_model(model, val_loader, threshold=THRESHOLD)
 
-        print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(e,
-                                                                 epoch_loss / len(train_loader)))
+        # after each epoch
+
+        print("===> Epoch {} Mean Dice: {} : Validation Loss: {:.4f}".format(e, mean_dice, val_loss))
