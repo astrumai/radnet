79c0c4e55f908a16fd0363533e9f62275b5fa051
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-10 19:08:44 -0800

    removed do augment function and moved into __getitem__

--- preprocessing/load.py
+++ preprocessing/load.py
@@ -33,20 +33,15 @@ class DataTransformer(Dataset):
         images = self._read_data(index)
         labels = self._read_labels(index)
 
-        if self.image_augmentation:
-            images, labels = self.do_augment(images, labels)
         if self.image_augmentation is not None:
-            images = self.image_transform(self._read_data(index))
-            labels = self.image_transform(self._read_labels(index))
-
-        return [images, labels]
-
-    def do_augment(self, images, labels):
-        images = np.array(images)
-        labels = np.array(labels)
-        res = self.image_augmentation(image=images, mask=labels)
-
-        return [Image.fromarray(res['image']), Image.fromarray(res['mask'])]
-
-
+            print("Aug")
+            image_array = np.array(images)
+            label_array = np.array(labels)
+            aug_images = self.image_augmentation(image=image_array, mask=label_array)
+            trans_images = Image.fromarray(aug_images["image"])
+            trans_labels = Image.fromarray(aug_images["mask"])
+        if self.image_augmentation is None:
+            trans_images = self.image_transform(images)
+            trans_labels = self.image_transform(labels)
+        return [trans_images, trans_labels]
 
