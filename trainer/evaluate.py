e342cdb842df8a79fe12483492aca095378cded0
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-12 14:26:48 -0800

    moved validate model to eval

--- trainer/evaluate.py
+++ trainer/evaluate.py
@@ -3,10 +3,39 @@ from torchvision.transforms import Compose, Resize, ToTensor
 from processing.load import DataTransformer
 from torch.utils.data import DataLoader
 import torch
-from utils.helpers import pred_to_numpy
+import numpy as np
+from utils.metrics import dice
+from utils.helpers import pred_to_numpy, to_numpy
+from torch import nn
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+loss_bce = nn.BCEWithLogitsLoss()
+
+
+def validate_model(model, loader, threshold):
+    """ Contains the validation loop
+    :param model:
+    :param loader:
+    :param threshold:
+    :return:
+    """
+    model = model.eval()
+    for i, data in enumerate(loader):
+        val_batch, val_labels = data
+        val_batch, val_labels = val_batch.to(device), val_labels.to(device)
+
+        # call the u-net module
+        prediction = model(val_batch)
+
+        # call the loss function
+    loss = loss_bce(prediction, val_labels)
+    pred_labels, true_labels = pred_to_numpy(prediction) > threshold, to_numpy(val_labels) > threshold
+    dices = []
+    for _pred, _labels in zip(pred_labels, true_labels):
+        dices.append(dice(_pred, _labels))
+    return np.array(dices).mean(), loss.detach().cpu().numpy()
 
 
 def evaluate(args):
