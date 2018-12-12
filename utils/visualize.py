3a9db2dfdcefdd72419ad2350c1c5d4bc730aeb3
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-12 15:48:45 -0800

    changes visualize.py to interpret

--- utils/visualize.py
+++
@@ -1,4 +0,0 @@
-# This is the module for interpretability
-# you have the code for visualizing the hidden layers of the u-net
-
-
---
+++ visualize/interpret.py
@@ -0,0 +1,5 @@
+"""This is the module for interpretability
+you have the code for visualizing the hidden layers of the u-net
+"""
+
+
