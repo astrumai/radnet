6a3b90c4c79f1a2ad088251f390c85397996dfc0
Mukesh Mithrakumar <mukesh.mithrakumar@jacks.sdstate.edu>
2018-12-24 19:59:10 -0800

    housekeeping

--- trainer/task.py
+++ trainer/task.py
@@ -107,14 +107,14 @@ if __name__ == '__main__' and __package__ is None:
                         )
 
     parser.add_argument('--log',
-                        default='yes',
+                        default='no',
                         choices=['yes', 'no'],
                         type=str,
                         help='Log the Values'
                         )
 
     parser.add_argument('--build_graph',
-                        default='yes',
+                        default='no',
                         choices=['yes', 'no'],
                         type=str,
                         help='Build the model graph'
